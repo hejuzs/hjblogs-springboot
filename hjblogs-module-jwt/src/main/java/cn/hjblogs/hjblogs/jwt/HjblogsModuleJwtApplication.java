aef8513753015ba16cf38d3573c782bbf1a15bc1
JUHE <13966915864@163.com>
2025-07-15 16:16:49 +0800

    整合 Spring Security

---
+++ hjblogs-module-jwt/src/main/java/cn/hjblogs/hjblogs/jwt/HjblogsModuleJwtApplication.java
@@ -0,0 +1,13 @@
+package cn.hjblogs.hjblogs.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjblogsModuleJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HjblogsModuleJwtApplication.class, args);
+    }
+
+}
---
+++ hjblogs-module-jwt/src/test/java/cn/hjblogs/hjblogs/jwt/HjblogsModuleJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hjblogs.hjblogs.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjblogsModuleJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
